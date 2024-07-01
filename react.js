1fadd052c9451c59faba6b342d7bf34d3b0926fc
sumit <sumit.accuro@gmail.com>
2024-07-01 14:36:08 -0400

    second commit

---
+++ react.js
@@ -0,0 +1,16 @@
+import "./App.css";
+
+export default function App() {
+  return (
+  <div>
+      <h1>Debouncing</h1>
+    <input
+      value={query}
+      type = "text"
+      onChange= {handleChange}
+      placeholder=
+      >
+    </input>
+  </div>
+  );
+}
